5de238a9c3bd88e8963673908c1ab002fad2ac62
kschultz703 <64384458+kschultz703@users.noreply.github.com>
2025-02-21 18:53:57 +0000

    added sort selects

--- client/src/components/Tasks.js
+++ client/src/components/Tasks.js
@@ -3,17 +3,37 @@ import {useState} from "react";
 
 function TaskForm({ tasks }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
-    const [selelectedSort, setSelectedSort] = useState()
-    
-    const updateSelectedCategory = (event) => {
-        setSelectedCategory(event.target.value);
-    };
+  const [selelectedSort, setSelectedSort] = useState("dueDateDescending")
+  
+  const updateSelectedCategory = (event) => {
+      setSelectedCategory(event.target.value);
+  };
 
-    const updateSelectedSort = (event) => {
-        setSelectedSort(event.target.value)
-    }
+  const updateSelectedSort = (event) => {
+      setSelectedSort(event.target.value)
+  }
+
   return (
     <>
+      <h1>To-Do List</h1>
+      <select
+        onChange={updateSelectedCategory}>
+        <option value="All">All Categories</option>      
+        <option value="Appointment">Appointment</option> 
+        <option value="Event">Event</option>
+        <option value="School">School</option>
+        <option value="Social">Social</option>
+        <option value="Work">Work</option>
+      </select>
+      <select
+        onChange={updateSelectedSort}>
+        <option value="dueDateDescending">Due Sooner</option>
+        <option value="dueDateAscending">Due Later</option>
+        <option value="priorityDescending">Priority: Highest to Lowest</option>      
+        <option value="priorityAscending">Priority: Lowest to Highest</option> 
+        <option value="alphabetDescending">Alphabetical: Highest to Lowest</option>
+        <option value="alphabetAscending">Alphabetical: Lowest to Highest</option>
+      </select>
       {tasks.map((task) => (
         <TaskCard {...task} />
       ))}
