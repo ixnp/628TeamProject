8c5816afa57a61168697f8c89db06187e890fe40
kschultz703 <64384458+kschultz703@users.noreply.github.com>
2025-02-21 18:47:04 +0000

    remove tasklist

--- client/src/components/Tasks.js
+++ client/src/components/Tasks.js
@@ -1,5 +1,17 @@
 import TaskCard from "./TaskCard";
+import {useState} from "react";
+
 function TaskForm({ tasks }) {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+    const [selelectedSort, setSelectedSort] = useState()
+    
+    const updateSelectedCategory = (event) => {
+        setSelectedCategory(event.target.value);
+    };
+
+    const updateSelectedSort = (event) => {
+        setSelectedSort(event.target.value)
+    }
   return (
     <>
       {tasks.map((task) => (
