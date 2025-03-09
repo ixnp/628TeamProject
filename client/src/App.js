6dbb9a5e8f55f5005f3bd97e565ee5293ab77b0e
kschultz703 <64384458+kschultz703@users.noreply.github.com>
2025-03-09 18:46:57 +0000

    get tasks

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,38 @@
 import TaskForm from "./components/CreateTaskPage";
 import Tasks from "./components/TaskForm";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {tasksTemp} from "./data/TempTasks";
+import { backendURL } from "./data/backendURL";
 
 
 function App() {
   const [formRendered, setformRendered] = useState("");
   const [taskStates, setTasks] = useState(tasksTemp);
 
-  const addTask = (newTask) => {
-    setTasks([...taskStates, newTask]);
-  }
+  useEffect(() => {
+    async function getTasks() {
+      const response = await fetch(`${backendURL}`);
+
+      if (!response.ok) {
+        const message = `An error occurred: ${response.statusText}`;
+        window.alert(message);
+        return;
+      }
+  
+      const tasks = await response.json();
+      setTasks(tasks)
+    }
+
+    getTasks();
+  
+    return;
+  }, [taskStates.length]);
+
 
   //Renders Task Form
   function CreateNewTaskButton() {
     function handleClick() {
-      return setformRendered(<TaskForm updateTasks={addTask} />);
+      return setformRendered(<TaskForm/>);
     }
 
     return <button onClick={handleClick}>Create New Task</button>;
